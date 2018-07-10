001a3b78c52babe819a9c92d644faee5619060d7
md0707 <ppuggy1@gmail.com>
2018-07-10 19:49:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,11 @@
 def fizzbuzz(marisa)
-  if marisa % 3 == 0
+  if marisa % 3 , 5 == 0
+    "Fizzbuzz"
+  elsif marisa % 3 == 0
     "Fizz"
+  elsif marisa % 5 == 0
+  "Buzz"
   end
-end
 
 fizzbuzz(3)
 fizzbuzz(5)
